c833880673e8e84c7e9161c04e070758f8ae1adc
t-alvamo <moises.alvarado@autodesk.com>
2021-12-08 13:59:40 -0600

    some refactor

--- src/App.js
+++ src/App.js
@@ -297,7 +297,7 @@ const App = () => {
         
 
         <div className="mt-4">
-          <Button size="sm" variant="warning" className="mr-4" onClick={(_) => send({ type: "RESET" })}>Reset Board</Button>
+          <Button size="sm" variant="warning" className="mr-4" onClick={() => send({ type: "RESET" })}>Reset Board</Button>
           <Button size="sm" variant="warning" className="mr-4" onClick={resetScore}>Reset Score</Button>
           <Button size="sm" variant="danger" onClick={handleShow2}>Edit / Remove Players</Button>
         </div>
